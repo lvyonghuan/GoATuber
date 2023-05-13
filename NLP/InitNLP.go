7c5c1257673a217a862fca23257097eaa8a5abee
lvyonghuan <110887355+lvyonghuan@users.noreply.github.com>
2023-05-13 11:20:45 +0800

    Modify initialization message read
    现在在打开live2d页面的时候初始化才会进行，避开了程序阻塞的情况。

--- NLP/InitNLP.go
+++ NLP/InitNLP.go
@@ -4,14 +4,12 @@ import (
 	"GoTuber/NLP/config"
 	"GoTuber/NLP/service"
 	"GoTuber/NLP/service/gpt"
-	"GoTuber/frontend/live2d_backend"
 )
 
 func InitNLP() {
 	service.HandelMsg.IsUse = false
 	go service.ChooseMessage()
 	service.ReadToGetFlag <- true
-	backend.WebsocketToNLP <- true //初始化NLP信息读取模块
 	go service.HandelMessage()
 	config.InitNLPConfig()
 	if config.NLPCfg.Nlp.UseGPT == true {
--- README.md
+++ README.md
@@ -10,5 +10,5 @@
 - 把live2d模型文件拖入 `dist\model` 中。具体参考这里的[README.md](https://github.com/lvyonghuan/GoATuber/tree/main/dist/model)。
 - 配置代理。config文件在proxy文件夹下。不配置代理，可能无法正常调用OpenAI等服务。
 - 运行 `cmd.go`。
-- 浏览器打开 `http://127.0.0.1:9000/` 。因为程序依靠管道进行信息传输控制，运行程序后不及时（具体来说，在文本生成并向前端传输之前）打开这个页面会造成程序阻塞。如果出现这种情况，请重新运行程序。
+- 浏览器打开 `http://127.0.0.1:9000/` 。
 - Enjoy！
