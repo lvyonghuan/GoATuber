3be656bfae3c31c1b59617c2aa30920ee171d453
lvyonghuan <110887355+lvyonghuan@users.noreply.github.com>
2023-05-28 23:53:37 +0800

    maybe fix bug
    但是我不知道

--- MEMORY/LLMmodel/gpt/gpt_memory.go
+++ MEMORY/LLMmodel/gpt/gpt_memory.go
@@ -15,10 +15,12 @@ func (chat Chat) StoreMessage() {
 	doc := pinecone.Memory.NewDocument(chat.Human, chat.UserName)
 	if err := pinecone.Memory.StoreDocument(doc); err != nil {
 		log.Println("存储用户消息失败，错误信息：", err)
+		return
 	}
 	doc = pinecone.Memory.NewDocument(chat.AI, "你")
 	if err := pinecone.Memory.StoreDocument(doc); err != nil {
 		log.Println("存储AI消息失败，错误信息：", err)
+		return
 	}
 }
 
@@ -26,6 +28,7 @@ func (chat Chat) GetMemory() (user, text string) {
 	docs, err := pinecone.Memory.RetrieveSimilarDocumentsByText(chat.Human, 1)
 	if err != nil {
 		log.Println("记忆获取失败，错误消息：", err)
+		log.Println(docs)
 	}
 	for _, d := range docs {
 		log.Println(d.User, d.Text)
