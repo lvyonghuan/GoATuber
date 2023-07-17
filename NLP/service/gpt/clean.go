ebd20b2632262caf19a59e12d26c971ea7d8fc8b
Lv Yonghuan <110887355+lvyonghuan@users.noreply.github.com>
2023-07-17 10:32:08 +0800

    nothing
    只是忘了去掉某些东西了

--- NLP/service/gpt/clean.go
+++ NLP/service/gpt/clean.go
@@ -1,7 +1,5 @@
 package gpt
 
-import "log"
-
 //清理消息记录，减少token使用量
 
 // 清除每次附带在消息上的记忆
@@ -17,5 +15,4 @@ func cleanAllMessage() {
 // 队列式的清除消息。达到token限制之后，清除队首的消息即可。
 func cleanFirstMessage() {
 	MS = append(MS[:roleLine], MS[roleLine+1:]...)
-	log.Println(MS[:roleLine-1], MS[roleLine+1:])
 }
