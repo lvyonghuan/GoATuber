2812e7ec5fba1ecf98976011d9de6361fbbdbd37
Lv Yonghuan <110887355+lvyonghuan@users.noreply.github.com>
2023-07-30 21:19:22 +0800

    Update websocket.go
    建立确认机制

--- frontend/model_backend/websocket.go
+++ frontend/model_backend/websocket.go
@@ -13,6 +13,7 @@ import (
 
 var done = make(chan bool, 1)
 var WebsocketToNLP = make(chan bool, 1) //控制信息流入的速度
+var start = make(chan bool, 1)          //向前端发送连接建立的消息
 
 var Upgrade = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -29,6 +30,7 @@ func Start(c *gin.Context) {
 		return
 	}
 	WebsocketToNLP <- true //初始化NLP信息读取模块
+	start <- true          //初始化前端连接成功
 	go write(conn)
 	go read(conn)
 }
@@ -39,6 +41,12 @@ func write(conn *websocket.Conn) {
 	}()
 	for {
 		select {
+		case <-start:
+			//向前端发送连接成功信息
+			err := conn.WriteMessage(websocket.TextMessage, []byte("ok"))
+			if err != nil {
+				log.Fatalf("websocket连接建立失败：%v", err)
+			}
 		case msg := <-OutPutChan:
 			// 将OutMessage结构体转换为JSON格式
 			jsonMsg, err := json.Marshal(msg)
